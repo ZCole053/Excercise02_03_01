27667189b2d06e46355fe7520568d524e2bd240f
Unknown <zcole053@west-mec.org>
2018-09-19 15:27:16 -0700

    Setting up
    I added a file to upload to the server along with a .js modernizer. The modernizer is linked in to the musical scale php file. The MusicalScale file is set up with a html basic file and some php for an array

---
+++ MusicalScale.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Musical Scale</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+<body>
+    <h2>Musical Scale</h2>
+    <?php 
+    $musicalScale = array("do", "re", "me", "fa", "so", "la", "ti", "do");
+    $outputString = "The notes of the musical scale are: ";
+    ?>
+</body>
+</html>
